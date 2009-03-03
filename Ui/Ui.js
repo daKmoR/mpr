2e5f5ec71d8ea2cd85883aff83d7d6ace7d0304d
Thomas Allmer <thomas.allmer@webteam.at>
2009-03-03 14:53:54 +0100

    Ui now uses a render function

--- Ui/Ui.js
+++ Ui/Ui.js
@@ -11,21 +11,26 @@
 var Ui = new Class({
 	Implements: [Events, Options],
 	options: {
+		theme: '',
 		Tabs: { param: '.ui-tab' },
 		Window: { param: '.ui-window' }
 	},
 	
 	Uis: {},
 	
-	initialize: function(theme, options) {
+	initialize: function(options) {
 		this.setOptions(options);
 		
+		if(this.options.theme != '')
+			this.render(this.options.theme);
+	},
+	
+	render: function(theme) {
 		$each( Ui[theme], function(el, name) {
 			if( $type(el) == 'class' ) {
 			  this.Uis[name] = new Ui[theme][name]( this.options[name].param, this.options[name].options );
 			}
 		}, this);
-		
 	}
 	
 });
