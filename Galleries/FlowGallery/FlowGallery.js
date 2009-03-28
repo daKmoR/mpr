10556ff2e1ba0b86bc20066a4b60d4ab88f9e178
Thomas Allmer <thomas.allmer@webteam.at>
2009-03-28 18:11:53 +0100

    more options for FlowGallery

--- Galleries/FlowGallery/FlowGallery.js
+++ Galleries/FlowGallery/FlowGallery.js
@@ -8,14 +8,39 @@
  * @copyright Copyright belongs to the respective authors
  */
  
+$require(MPR.path + 'More/Fx.Slide/Fx.Slide.js');
+ 
 FlowGallery = new Class({
 	Implements: [Options, Events],
 	options: {
 		start: 0,
-		auto: true,
-		autointerval: 4000,
-		onShow: function(title, container) { title.addClass('active'); container.fade(1); },
-		onHide: function(title, container) { title.removeClass('active'); container.fade(0); }
+		auto: false,
+		duration: 4000,
+		mode: 'continious',
+		increase: 1,
+		onShow: function(image, title, container) { 
+			this.options.effects[this.options.usefx.getRandom()]( image, 'in' );
+		},
+		onHide: function(image, title, container) { 
+			this.options.effects[this.options.usefx.getRandom()]( image, 'hide' );
+			
+			//this.options.effects[this.options.usefx.getRandom()].delay( this.options.duration, this, [image, 'hide'] );
+		},
+		effect: {
+			active: ['slide'],
+			duration: 2000,
+		},
+		usefx: ['slide'],
+		effects: {
+			fade: function(el, state) {
+				el.set('tween', { 'duration': 2000 } );
+				el.fade( state == 'in' ? 1 : 0 );
+			},
+			slide: function(el, state) {
+				el.set('slide', { 'duration': 2000 } );
+				el.slide( state );
+			}
+		}
 	},
 	
 	containers: [],
@@ -31,11 +56,15 @@ FlowGallery = new Class({
 		this.containers = $$(container);
 		this.images = this.containers.getElement('img');
 		
-		console.log( this.images );
+		this.images.each( function(el, i) {
+			el.setStyle('z-index', i);
+			this.options.effects[this.options.usefx.getRandom()]( el, 'hide' );
+		}, this);
+		
+		this.images.setStyle('position', 'relative');
+		
 		this.titles = $$('.menu h3');
 
-		this.containers.fade('hide');					
-		
 		this.titles.each( function(el, i) {
 			el.addEvent('click', function(e) {
 				e.stop();
@@ -47,25 +76,46 @@ FlowGallery = new Class({
 	},
 	
 	show: function(id) {
-		this.fireEvent('onShow', [ this.titles[id], this.containers[id] ]);
+		this.titles[id].addClass('active');
+		this.images[id].setStyle('z-index', this.images.length + 1 );
+		
 		if( id != this.current) {
 			this.hide(this.current);
 			this.current = id;
 		}
+		this.fireEvent('onShow', [ this.images[id], this.titles[id], this.containers[id] ]);
 		if( this.options.auto ) this.auto();
 	},
 	
 	hide: function(id) {
-		this.fireEvent('onHide', [ this.titles[id], this.containers[id] ]);
+		this.titles[id].removeClass('active');
+		this.images[id].setStyle('z-index', id);
+		$clear(this.hidefxtimer);
+		this.hidefxtimer = this.hidefx.delay(this.options.effect.duration, this, this.current);
+	},
+	
+	hidefx: function(id) {
+		this.fireEvent('onHide', [ this.images[id], this.titles[id], this.containers[id] ]);
 	},
 	
 	auto: function() {
 		$clear(this.autotimer);
-		this.autotimer = this.progress.delay(this.options.autointerval, this);
+		this.autotimer = this.next.delay(this.options.duration, this);
 	},
 	
-	progress: function() {
-		this.show( (this.current + 1 < this.containers.length) ? this.current + 1 : 0);
+	next: function(increase) {
+		var increase = increase || this.options.increase;
+		var next = 0;
+		if ( this.options.mode === 'reverse' ) increase *= -1;
+			
+		if ( this.options.mode === 'random' ) {
+			next = $random(0, this.containers.length-1 );
+		} else {
+			if ( this.current + increase < this.containers.length ) next = this.current + increase;
+			if ( this.current + increase < 0 )	next = this.containers.length-1;
+		}
+		
+		this.show(next);
 	}
 	
 });
