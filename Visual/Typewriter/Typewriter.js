a9d24a2c3edcd6e7a37fd27e60334090c545f4e2
Thomas Allmer <thomas.allmer@webteam.at>
2009-04-05 23:14:57 +0200

    restart whatever you want...

--- Visual/Typewriter/Typewriter.js
+++ Visual/Typewriter/Typewriter.js
@@ -1,10 +1,9 @@
 /**
- * a nice visual effect of writing
+ * a nice visual effect of writing (original idea by david walsh)
  *
  * @version		0.0.1
  *
  * @license		MIT-style license
- * @author		david walsh
  * @author		Thomas Allmer <at@delusionworld.com>
  * @copyright Copyright belongs to the respective authors
  */
@@ -13,12 +12,13 @@ var Typewriter = new Class({
 	Implements: [Options],
 	options: {
 		message: '',
-		delay: 150,
+		duration: 150,
 		cursor: 0,
 		auto: true,
 		step: 1
 	},
 	
+	autotimer: $empty,
 	container: $empty,
 	
 	initialize: function(element, options) {
@@ -27,28 +27,25 @@ var Typewriter = new Class({
 		this.options.message = this.options.message || this.container.get('html');
 		
 		this.reset();
-		if (this.options.auto) this.start();
+		if (this.options.auto) this.auto();
+	},
+	
+	auto: function() {
+		$clear(this.autotimer);
+		this.autotimer = this.setLetter.delay(this.options.duration, this);
 	},
 	
 	reset: function() {
 		this.container.set('html', '');
 		this.options.cursor = 0;
-		if (this.options.auto) this.start();
+		if (this.options.auto) this.auto();
 	},
 	
-	start: function() {
-		//for every letter
-		for(x = 0; x < this.options.message.length; x++) {
-			var id = this.setLetter.delay(this.options.delay * x, this);
-		}
-	},
-	
-	//place the newest letter in the container
 	setLetter: function(cursor) {
-		if (cursor)
-			this.options.cursor = cursor;
+		if (cursor) this.options.cursor = cursor;
 		this.container.set('html', this.options.message.substring(0, this.options.cursor) );
 		this.options.cursor += this.options.step;
+		if (this.options.auto) this.auto();
 	}
 	
 });
