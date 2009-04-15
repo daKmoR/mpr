f0c11adc8b4b5cf03f337cd66a944c4a06aa967a
Thomas Allmer <daKmoR@gmx.at>
2009-04-15 10:36:10 +0200

    MprConfig example

---
+++ MprConfig.example.php
@@ -0,0 +1,13 @@
+<?php
+
+	// here you can override the default config from Mpr\Php\MprConfig.php
+	// just look it up what the config names are...
+	// once you defined your options rename this file from MprConfig.examle.php to MprConfig.php
+
+	// for example if you don't want to use cache
+	$MprOptions['useCache'] = false;
+	
+	// or you don't want to use gzip
+	$useGzip = false; 
+	
+?>
