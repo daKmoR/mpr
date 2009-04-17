3011a725d53306552fc152e1ffc626a3e9b6c422
Thomas Allmer <daKmoR@gmx.at>
2009-04-17 14:13:09 +0200

    new MPR Plugin MultiOverlays

---
+++ Visual/MultiOverlays/MultiOverlay.js
@@ -0,0 +1,72 @@
+/**
+ * create multiple overlays from images with an imagemap
+ *
+ * @version		0.0.1
+ *
+ * @license		MIT-style license
+ * @author		Thomas Allmer <at@delusionworld.com>
+ * @copyright Copyright belongs to the respective authors
+ */
+
+var MultiOverlay = new Class({
+	Implements: [Events, Options],
+	
+	options: {
+		activeClass: 'active',
+		onInit: function(overlay, active) {
+			if ( active )
+				overlay.addClass( this.options.activeClass );
+			else
+				overlay.fade('hide');
+		},
+		onShow: function(overlay) {
+			overlay.fade(1);
+		},
+		onHide: function(overlay) {
+			overlay.fade(0);
+		}
+	},
+	
+	initialize: function(source, options) {
+		this.setOptions(options);
+		this.source = $(source);
+		this.build();
+	},
+	
+	build: function() {
+		this.areas = $$(this.source.get('usemap'))[0].getElements('area');
+		this.container = new Element('div', { style: 'position: relative;' }).wraps(this.source);
+		
+		$each( this.areas, function(el) {
+			this.attach(el);
+		}, this);
+	},
+	
+	attach: function(area) {
+		var overlay = new Element('img', { 
+			src: area.get('title'), 
+			alt: area.get('alt'), 
+			style: 'position: absolute; left: 0; top: 0;', 
+			usemap : this.source.get('usemap') 
+		});
+		area.erase('title');
+		this.fireEvent('onInit', [overlay, area.hasClass(this.options.activeClass)] );
+		this.container.grab(overlay);
+		
+		area.store('MultiOverlay:Image', overlay);
+		var that = this;
+		area.addEvents({
+			'mouseenter': function() {
+				var overlay = this.retrieve('MultiOverlay:Image');
+				if( !overlay.hasClass(that.options.activeClass) )
+					that.fireEvent('onShow', [overlay] );
+			},
+			'mouseleave': function() {
+				var overlay = this.retrieve('MultiOverlay:Image');
+				if( !overlay.hasClass(that.options.activeClass) )
+					that.fireEvent('onHide', [overlay] );
+			}
+		});
+	}
+	
+});
