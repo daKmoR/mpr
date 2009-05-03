eb25b5f8a6093a4abcc80c7ce60de0e63c61ef23
Thomas Allmer <daKmoR@gmx.at>
2009-05-03 13:32:42 +0200

    use a dummy Request for the MprCore

--- Mpr/MprCore.js
+++ Mpr/MprCore.js
@@ -16,22 +16,23 @@ if ( typeof(MPR.path) === 'undefined' )
 if ( typeof(MPR.files) === 'undefined' )
 	MPR.files = [];
 	
+function Request(){};
+	
 function $require(source) {
 	if ( MPR.files[source] ) return true;
 	MPR.files[source] = 1;
 	
 	switch ( source.match(/\.\w+$/)[0] ) {
 		case '.js': {
-			// new Request({ url: source, async: false, evalResponse: true, method: 'get',
-				// onComplete: function() {
-					// return new Element('script', {src: source, type: 'text/javascript'}).inject(document.head);
-				// }
-			// }).send();
+			new Request({ url: source, async: false, evalResponse: true, method: 'get',
+				onComplete: function() {
+					return new Element('script', {src: source, type: 'text/javascript'}).inject(document.head);
+				}
+			}).send();
 			return true;
 		}
 		case '.css': {
-			//return new Element('link', { rel: 'stylesheet', media: 'screen', type: 'text/css', href: source }).inject(document.head);
-			return true;
+			return new Element('link', { rel: 'stylesheet', media: 'screen', type: 'text/css', href: source }).inject(document.head);
 		}
 	}
 	
