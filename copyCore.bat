f57e31adab7836395eb13804f2d1512525517dca
Thomas Allmer <daKmoR@gmx.at>
2009-05-11 16:16:38 +0200

    updated copyCore.bat script to renamve Fx.CSS.js to Fx.Css; same fore Request.HTML and Request.JSON

--- copyCore.bat
+++ copyCore.bat
@@ -18,8 +18,10 @@ cp %CORE%Docs/Element/* %MPR%Core/Element/Docu/ -f
 
 REM Fx
 cp %CORE%Source/Fx/* %MPR%Core/Fx/ -f
+mv %MPR%Core/Fx/Fx.CSS.js %MPR%Core/Fx/Fx.Css.js
 
 cp %CORE%Docs/Fx/* %MPR%Core/Fx/Docu/ -f
+mv %MPR%Core/Fx/Docu/Fx.CSS.md %MPR%Core/Fx/Docu/Fx.Css.md
 
 REM Native
 cp %CORE%Source/Native/* %MPR%Core/Native/ -f
@@ -27,13 +29,13 @@ cp %CORE%Source/Native/* %MPR%Core/Native/ -f
 cp %CORE%Docs/Native/* %MPR%Core/Native/Docu/ -f
 
 REM Request
-cp %CORE%Source/Request/Request.HTML.js %MPR%Core/Request/Request.Html.js -f
-cp %CORE%Source/Request/Request.js %MPR%Core/Request/Request.js -f
-cp %CORE%Source/Request/Request.JSON.js %MPR%Core/Request/Request.Json.js -f
+cp %CORE%Source/Request/* %MPR%Core/Request/ -f
+mv %MPR%Core/Request/Request.HTML.js %MPR%Core/Request/Request.Html.js
+mv %MPR%Core/Request/Request.JSON.js %MPR%Core/Request/Request.Json.js
 
-cp %CORE%Docs/Request/Request.HTML.md %MPR%Core/Request/Docu/Request.Html.md -f
-cp %CORE%Docs/Request/Request.md %MPR%Core/Request/Docu/Request.md -f
-cp %CORE%Docs/Request/Request.JSON.md %MPR%Core/Request/Docu/Request.Json.md -f
+cp %CORE%Docs/Request/* %MPR%Core/Request/Docu/ -f
+mv %MPR%Core/Request/Docu/Request.HTML.md %MPR%Core/Request/Docu/Request.Html.md
+mv %MPR%Core/Request/Docu/Request.JSON.md %MPR%Core/Request/Docu/Request.Json.md
 
 REM Utilities
 cp %CORE%Source/Utilities/* %MPR%Core/Utilities/ -f
