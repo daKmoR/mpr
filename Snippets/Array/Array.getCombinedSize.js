4b3a00b205ad40fe258e18118dc0a7e25017beb7
Thomas Allmer <daKmoR@gmx.at>
2009-09-07 01:15:28 +0200

    some conde optimization

--- Snippets/Array/Array.getCombinedSize.js
+++ Snippets/Array/Array.getCombinedSize.js
@@ -1,9 +1,10 @@
 Array.implement({
 	getCombinedSize: function(){
-		var elsWidth = 0, elsHeight = 0;
+		var elsWidth = 0, elsHeight = 0, size;
 		for (var i = 0, l = this.length; i < l; i++){
-			elsWidth += this[i].getSize().x;
-			elsHeight += this[i].getSize().y;
+			size = this[i].getSize();
+			elsWidth += size.x;
+			elsHeight += size.y;
 		}
 		return {x: elsWidth, y: elsHeight};
 	}
