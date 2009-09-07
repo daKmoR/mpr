8b48a4229f1e9ace81775cc9d2dba91999cdaa04
Thomas Allmer <daKmoR@gmx.at>
2009-09-07 02:34:40 +0200

    getCombinedSize now also takes into account margin

--- Snippets/Array/Array.getCombinedSize.js
+++ Snippets/Array/Array.getCombinedSize.js
@@ -1,10 +1,11 @@
 Array.implement({
-	getCombinedSize: function(){
+	getCombinedSize: function(margin){
+		var margin = margin || true;
 		var elsWidth = 0, elsHeight = 0, size;
 		for (var i = 0, l = this.length; i < l; i++){
 			size = this[i].getSize();
-			elsWidth += size.x;
-			elsHeight += size.y;
+			elsWidth += size.x + (margin ? this[i].getStyle('margin-left').toInt() + this[i].getStyle('margin-right').toInt() : 0);
+			elsHeight += size.y + (margin ? this[i].getStyle('margin-top').toInt() + this[i].getStyle('margin-bottom').toInt() : 0);
 		}
 		return {x: elsWidth, y: elsHeight};
 	}
