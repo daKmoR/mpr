81cc67ee09d6eac9e60861c343cd0d332e1c030b
Thomas Allmer <daKmoR@gmx.at>
2009-09-08 02:21:17 +0200

    fixed slides so they take margin into account added slideTop and slideBottom

--- Galleries/FlexSlide/FlexSlide.Transitions.js
+++ Galleries/FlexSlide/FlexSlide.Transitions.js
@@ -17,22 +17,38 @@ FlexSlide.implement('options', {
 			slideLeftBounce: { transition: Fx.Transitions.Bounce.easeOut },
 			slideRightBounce: { transition: Fx.Transitions.Bounce.easeOut },
 			slideLeftQuart: { transition: Fx.Transitions.Quart.easeInOut },
-			slideRightQuart: { transition: Fx.Transitions.Quart.easeInOut }
+			slideRightQuart: { transition: Fx.Transitions.Quart.easeInOut },
+			slideTopQuart: { transition: Fx.Transitions.Quart.easeInOut },
+			slideBottomQuart: { transition: Fx.Transitions.Quart.easeInOut }
 		},
 	},
 	effects: {
 		slideLeft: function(current, next, currentEl, nextEl) {
-			this.fxConfig[current] = { 'left': [0, currentEl.getSize().x*-1] };
-			this.fxConfig[next]    = { 'left': [currentEl.getSize().x, 0] };
+			var width = currentEl.getSize().x + currentEl.getStyle('margin-left').toInt() + currentEl.getStyle('margin-right').toInt();
+			this.fxConfig[current] = { 'left': [0, width*-1] };
+			this.fxConfig[next]    = { 'left': [width, 0] };
 		},
 		slideRight: function(current, next, currentEl, nextEl) {
-			this.fxConfig[current] = { 'right': [0, currentEl.getSize().x*-1] };
-			this.fxConfig[next]    = { 'right': [currentEl.getSize().x, 0] };
+			var width = currentEl.getSize().x + currentEl.getStyle('margin-left').toInt() + currentEl.getStyle('margin-right').toInt();
+			this.fxConfig[current] = { 'right': [0, width*-1] };
+			this.fxConfig[next]    = { 'right': [width, 0] };
 		},
 		slideLeftBounce:  function(current, next, currentEl, nextEl) { this.options.effects.slideLeft.call (this, current, next, currentEl, nextEl); },
 		slideRightBounce: function(current, next, currentEl, nextEl) { this.options.effects.slideRight.call(this, current, next, currentEl, nextEl); },
 		slideLeftQuart:   function(current, next, currentEl, nextEl) { this.options.effects.slideLeft.call (this, current, next, currentEl, nextEl); },
-		slideRightQuart:  function(current, next, currentEl, nextEl) { this.options.effects.slideRight.call(this, current, next, currentEl, nextEl); }
+		slideRightQuart:  function(current, next, currentEl, nextEl) { this.options.effects.slideRight.call(this, current, next, currentEl, nextEl); },
+		slideTop: function(current, next, currentEl, nextEl) {
+			var height = currentEl.getSize().y + currentEl.getStyle('margin-top').toInt() + currentEl.getStyle('margin-bottom').toInt();
+			this.fxConfig[current] = { 'top': [0, height*-1] };
+			this.fxConfig[next]    = { 'top': [height, 0] };
+		},
+		slideBottom: function(current, next, currentEl, nextEl) {
+			var height = currentEl.getSize().y + currentEl.getStyle('margin-top').toInt() + currentEl.getStyle('margin-bottom').toInt();
+			this.fxConfig[current] = { 'bottom': [0, height*-1] };
+			this.fxConfig[next]    = { 'bottom': [height, 0] };
+		},
+		slideTopQuart:  function(current, next, currentEl, nextEl) { this.options.effects.slideTop.call(this, current, next, currentEl, nextEl); },
+		slideBottomQuart:  function(current, next, currentEl, nextEl) { this.options.effects.slideBottom.call(this, current, next, currentEl, nextEl); },
 	}
 
 });
