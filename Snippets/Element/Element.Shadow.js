af8e031a24909d9eea540712fb55adbd46806893
Thomas Allmer <daKmoR@gmx.at>
2009-09-13 05:01:27 +0200

    make Element.Shadow easily usable incl. demo

--- Snippets/Element/Element.Shadow.js
+++ Snippets/Element/Element.Shadow.js
@@ -1,28 +1,32 @@
 Element.implement({
 	shadow: function(_options) {
-		var options = {};
+		var options = { color: '#444444', opacity: 0.9, shadowBlur: 11, shadowSize: 12, shadowOffset: { x: 5, y: 5 }, wrap: { 'class': 'ui-Shadow' } };
 		$extend(options, _options);
 		var size = options.size || this.getSize();
-		if(Browser.Engine.trident){
-			this.setStyle('filter', 'progid:DXImageTransform.Microsoft.Shadow(color=#444444, direction=135, strength=10)');
-			return this;
+		var wrap = new Element('div', $extend(options.wrap, {'style': 'position: relative; width: ' + (size.x + options.shadowSize) + 'px; height: ' + (size.y + options.shadowSize) + 'px' }) ).wraps( this );
+		if( Browser.Engine.trident ) {
+			wrap.setStyle('filter', 'progid:DXImageTransform.Microsoft.Shadow(color=' + options.color + ', direction=135, strength=' + (options.shadowSize*0.8).round() +')');
 		} else {
 			var canvas = new Element('canvas', {
 				'width': size.x,
 				'height': size.y,
-				'style': 'width: 100%; height: 100%; position: absolute; right: -4%; bottom: -6%; z-index: -1'
+				'style': 'width: 100%; height: 100%; position: absolute; right: 0; bottom: 0; z-index: -1'
 			});
 			if( canvas.getContext ) {
 				var ctx = canvas.getContext('2d');
-				ctx.fillStyle = 'rgba(255, 255, 255, 0.9)';
-				ctx.shadowColor = '#444';
-				ctx.shadowOffsetX = 5;
-				ctx.shadowOffsetY = 5;
-				ctx.shadowBlur = 12;
-				ctx.fillRect(0, 0, size.x-12, size.y-12);
+				ctx.fillStyle = 'rgba(255, 255, 255, ' + options.opacity + ')';
+				ctx.shadowColor = options.color;
+				ctx.shadowOffsetX = options.shadowOffset.x;
+				ctx.shadowOffsetY = options.shadowOffset.y;
+				ctx.shadowBlur = options.shadowBlur;
+				ctx.fillRect(0, 0, size.x-options.shadowSize, size.y-options.shadowSize);
 			}
 			canvas.inject(this, 'after');
-			return canvas;
 		}
+		this.setStyles({
+			'width': (size.x/((size.x+options.shadowSize)/100)).round(2) + '%',
+			'height': (size.y/((size.y+options.shadowSize)/100)).round(2) + '%'
+		});
+		return this;
 	}
 });
